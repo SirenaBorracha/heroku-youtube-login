f797b0ec744c21ebf9cf3307d8059b4f1e913f1a
Joe Jackson <ph8tel@gmail.com>
2018-04-21 11:50:22 -0700

    added xorigin to api

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -18,6 +18,13 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
 router.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://mariner-front-end.s3-website.us-east-2.amazonaws.com")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
+
+router.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:8080")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     next()
